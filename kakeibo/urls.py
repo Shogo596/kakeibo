95da9133cdc742948c24424a304b95554a91e580
Shogo Ito <es4.d94b.596@gmail.com>
2020-01-12 22:26:29 +0900

    微調整

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 日別支出一覧画面への遷移
     path('', views.view_list, name='view_list'),
+    path('kakeibo/', views.view_list, name='view_list'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('blog.urls')),
     # path('blog/', include('blog.urls')),
-    path('kakeibo/', include('kakeibo.urls')),
+    path('', include('kakeibo.urls')),
+    # path('kakeibo/', include('kakeibo.urls')),
 ]
